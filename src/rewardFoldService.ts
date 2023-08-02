e734f95db737a24c5d83bdf464f46bd939900bf1
solidsnakedev <jona.ca.eng@gmail.com>
2023-08-02 11:46:49 -0400

    add reward-fold service, bump version

--- src/rewardFoldService.ts
+++ src/rewardFoldService.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { setTimeout } from "timers/promises";
+
 import {
   Blockfrost,
   Lucid,
@@ -27,48 +29,55 @@ const run = async () => {
     applied.scripts.discoveryValidator
   );
 
-  const rewardFoldConfig: RewardFoldConfig = {
-    nodeInputs: nodeUTxOs,
-    projectCS: process.env.PROJECT_CS!,
-    projectTN: process.env.PROJECT_TN!,
-    projectAddress: beneficiaryAddress,
-    scripts: {
-      nodeValidator: applied.scripts.discoveryValidator,
-      discoveryStake: applied.scripts.discoveryStake,
-      rewardFoldPolicy: applied.scripts.rewardPolicy,
-      rewardFoldValidator: applied.scripts.rewardValidator,
-    },
-    refScripts: {
-      nodeValidator: (
-        await lucid.utxosByOutRef([refScripts.scriptsRef.DiscoveryValidator])
-      )[0],
-      discoveryStake: (
-        await lucid.utxosByOutRef([
-          refScripts.scriptsRef.DiscoveryStakeValidator,
-        ])
-      )[0],
-      rewardFoldPolicy: (
-        await lucid.utxosByOutRef([refScripts.scriptsRef.RewardFoldPolicy])
-      )[0],
-      rewardFoldValidator: (
-        await lucid.utxosByOutRef([refScripts.scriptsRef.RewardFoldValidator])
-      )[0],
-    },
-  };
+  let rewardUTxOs = await utxosAtScript(lucid, applied.scripts.rewardValidator);
 
-  lucid.selectWalletFromSeed(process.env.WALLET_BENEFICIARY_1!);
-  const rewardFoldUnsigned = await rewardFold(lucid, rewardFoldConfig);
+  while (rewardUTxOs.length == 1) {
+    const rewardFoldConfig: RewardFoldConfig = {
+      nodeInputs: nodeUTxOs,
+      projectCS: process.env.PROJECT_CS!,
+      projectTN: process.env.PROJECT_TN!,
+      projectAddress: beneficiaryAddress,
+      scripts: {
+        nodeValidator: applied.scripts.discoveryValidator,
+        discoveryStake: applied.scripts.discoveryStake,
+        rewardFoldPolicy: applied.scripts.rewardPolicy,
+        rewardFoldValidator: applied.scripts.rewardValidator,
+      },
+      refScripts: {
+        nodeValidator: (
+          await lucid.utxosByOutRef([refScripts.scriptsRef.DiscoveryValidator])
+        )[0],
+        discoveryStake: (
+          await lucid.utxosByOutRef([
+            refScripts.scriptsRef.DiscoveryStakeValidator,
+          ])
+        )[0],
+        rewardFoldPolicy: (
+          await lucid.utxosByOutRef([refScripts.scriptsRef.RewardFoldPolicy])
+        )[0],
+        rewardFoldValidator: (
+          await lucid.utxosByOutRef([refScripts.scriptsRef.RewardFoldValidator])
+        )[0],
+      },
+    };
 
-  if (rewardFoldUnsigned.type == "error") {
-    console.log(rewardFoldUnsigned.error);
-    return;
+    lucid.selectWalletFromSeed(process.env.WALLET_BENEFICIARY_1!);
+    const rewardFoldUnsigned = await rewardFold(lucid, rewardFoldConfig);
+
+    if (rewardFoldUnsigned.type == "error") {
+      console.log(rewardFoldUnsigned.error);
+      return;
+    }
+
+    // console.log(initNodeUnsigned.data.txComplete.to_json());
+    const rewardFoldSigned = await rewardFoldUnsigned.data.sign().complete();
+    const rewardFoldHash = await rewardFoldSigned.submit();
+    await lucid.awaitTx(rewardFoldHash);
+    console.log("submitted TxHash: ", rewardFoldHash);
+
+    await setTimeout(20_000);
+    rewardUTxOs = await utxosAtScript(lucid, applied.scripts.rewardValidator);
   }
-
-  // console.log(initNodeUnsigned.data.txComplete.to_json());
-  const rewardFoldSigned = await rewardFoldUnsigned.data.sign().complete();
-  const rewardFoldHash = await rewardFoldSigned.submit();
-  await lucid.awaitTx(rewardFoldHash);
-  console.log("submitted TxHash: ", rewardFoldHash);
 };
 
 await run();
