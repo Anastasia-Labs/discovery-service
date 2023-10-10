9c8568278ca5a0b841ef0e6dad0a473349cb7175
Calvin Koepke <hello@calvinkoepke.com>
2023-10-10 14:56:39 -0600

    chore: updates

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Setting Up a Test Test
 
-- Run `yarn ts-node --esm src/utils/createWallets.ts`
+- Run `yarn create-wallets`
 - Update the `.env` file with wallet seeds.
 - Fund wallet Project 0 with testnet funds.
-- Then run `yarn ts-node --esm src/utils/fundWallets.ts` to fund other wallets.
-- Then create a new test token with `yarn ts-node --esm src/utils/mintToken.ts` (change the token name before executing).
+- Then run `yarn fund-wallets` to fund other wallets.
+- Then create a new test token with `yarn mint-token` (change the token name before executing).
 - Update the `.env` file with token policy ID and token name.
-- Then run `yarn ts-node --esm src/service/buildScripts.ts`
-- Once `./applied-scripts.json` file is created, run `yarn ts-node --esm src/service/deployScripts.ts` (**NOTE**: This takes quite a while.)
-- Once that is done, run `yarn ts-node --esm src/service/initializeDiscovery.ts`
+- Then run `yarn build-scripts`
+- Once `./applied-scripts.json` file is created, run `yarn deploy-scripts` (**NOTE**: This takes quite a while.)
+- Once that is done, run `yarn initialize-discovery`
 - Your Taste Test is live!
--- src/utils/mintToken.ts
+++ src/utils/mintToken.ts
@@ -1,83 +1,89 @@
 import dotenv from "dotenv";
+import inquirer from "inquirer";
 dotenv.config();
 
 import {
-    Blockfrost,
-    fromText,
-    Lucid,
-    MintingPolicy,
-    PolicyId,
-    TxHash,
-    Unit,
-    Network
-  } from "lucid-cardano";
+  Blockfrost,
+  fromText,
+  Lucid,
+  MintingPolicy,
+  PolicyId,
+  TxHash,
+  Unit,
+  Network
+} from "lucid-cardano";
 
-  const TEST_TOKEN_NAME = "SSLTest1"
-  
-  const lucid = await Lucid.new(
-    new Blockfrost(process.env.API_URL!, process.env.API_KEY),
-    process.env.NETWORK as Network
-  );
-  
-  lucid.selectWalletFromSeed(process.env.WALLET_PROJECT_0!)
-  
-  const { paymentCredential } = lucid.utils.getAddressDetails(
-    await lucid.wallet.address(),
-  );
-  
-  const mintingPolicy: MintingPolicy = lucid.utils.nativeScriptFromJson(
+const lucid = await Lucid.new(
+  new Blockfrost(process.env.API_URL!, process.env.API_KEY),
+  process.env.NETWORK as Network
+);
+
+lucid.selectWalletFromSeed(process.env.WALLET_PROJECT_0!)
+
+const { paymentCredential } = lucid.utils.getAddressDetails(
+  await lucid.wallet.address(),
+);
+
+const mintingPolicy: MintingPolicy = lucid.utils.nativeScriptFromJson(
+  {
+    type: "all",
+    scripts: [
+      { type: "sig", keyHash: paymentCredential?.hash! },
+      {
+        type: "before",
+        slot: lucid.utils.unixTimeToSlot(Date.now() + 1000000),
+      },
+    ],
+  },
+);
+
+const policyId: PolicyId = lucid.utils.mintingPolicyToId(
+  mintingPolicy,
+);
+
+export async function mintNFT(): Promise<TxHash> {
+  const { value } = await inquirer.prompt<{ value: string }>([
     {
-      type: "all",
-      scripts: [
-        { type: "sig", keyHash: paymentCredential?.hash! },
-        {
-          type: "before",
-          slot: lucid.utils.unixTimeToSlot(Date.now() + 1000000),
-        },
-      ],
-    },
-  );
+      type: 'input',
+      name: 'value',
+      message: 'What do you want to name your token? (no spaces, one word)',
+    }
+  ]);
   
-  const policyId: PolicyId = lucid.utils.mintingPolicyToId(
-    mintingPolicy,
-  );
-  
-  export async function mintNFT(
-    name: string,
-  ): Promise<TxHash> {
-    const unit: Unit = policyId + fromText(name);
-  
-    const tx = await lucid
-      .newTx()
-      .mintAssets({ [unit]: 100000000n })
-      .validTo(Date.now() + 100000)
-      .attachMintingPolicy(mintingPolicy)
-      .complete();
-  
-    const signedTx = await tx.sign().complete();
-  
-    const txHash = await signedTx.submit();
-  
-    return txHash;
-  }
-  
-  export async function burnNFT(
-    name: string,
-  ): Promise<TxHash> {
-    const unit: Unit = policyId + fromText(name);
-  
-    const tx = await lucid
-      .newTx()
-      .mintAssets({ [unit]: -1n })
-      .validTo(Date.now() + 100000)
-      .attachMintingPolicy(mintingPolicy)
-      .complete();
-  
-    const signedTx = await tx.sign().complete();
-  
-    const txHash = await signedTx.submit();
-  
-    return txHash;
-  }
+  const unit: Unit = policyId + fromText(value);
 
-  mintNFT(TEST_TOKEN_NAME);
+  const tx = await lucid
+    .newTx()
+    .mintAssets({ [unit]: 100000000n })
+    .validTo(Date.now() + 100000)
+    .attachMintingPolicy(mintingPolicy)
+    .complete();
+
+  const signedTx = await tx.sign().complete();
+
+  const txHash = await signedTx.submit();
+
+  console.log(`Minted token ${value} under policy ID (${mintingPolicy.script}). TxHash: ${txHash}`)
+  return txHash;
+}
+
+export async function burnNFT(
+  name: string,
+): Promise<TxHash> {
+  const unit: Unit = policyId + fromText(name);
+
+  const tx = await lucid
+    .newTx()
+    .mintAssets({ [unit]: -1n })
+    .validTo(Date.now() + 100000)
+    .attachMintingPolicy(mintingPolicy)
+    .complete();
+
+  const signedTx = await tx.sign().complete();
+
+  const txHash = await signedTx.submit();
+
+  return txHash;
+}
+
+mintNFT();
